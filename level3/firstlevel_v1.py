9440b8cb80aa8db7a9e6c0b5baa000c13a8ac333
katerinka228 <mailto:varfolomeewa.catherine@yandex.ru>
2023-01-17 20:54:35 +0300

    третий уровень

--- level3/firstlevel_v1.py
+++ level3/firstlevel_v1.py
@@ -18,14 +18,14 @@ retry_text = font_small.render('press to restart', True, (255, 255, 255))
 retry_rect = retry_text.get_rect()
 retry_rect.midtop = (W // 2, H // 2)
 
-ground_image = pygame.image.load('floor11.png')
+ground_image = pygame.image.load('floor.png')
 ground_image = pygame.transform.scale(ground_image, (1024, 80))
 GROUND_H = ground_image.get_height()
 
 player_image = pygame.image.load('skeleton.png.')
 player_image = pygame.transform.scale(player_image, (100, 200))
 
-bg_image = pygame.image.load('level1_bg.jpg')
+bg_image = pygame.image.load('level3_bg.jpg')
 gr_speed = 3
 
 enemy_image = pygame.image.load('tomb.png')
@@ -33,12 +33,6 @@ enemy_image = pygame.transform.scale(enemy_image, (100, 100))
 
 enemy_dead_image = pygame.image.load('dead_tomb.png')
 enemy_dead_image = pygame.transform.scale(enemy_dead_image, (100, 100))
-
-# grob_image = pygame.image.load('grob.png')
-# grob_image = pygame.transform.scale(grob_image, (130, 230))
-#
-# coin_image = pygame.image.load('coin.png')
-# coin_image = pygame.transform.scale(coin_image, (50, 50))
 
 
 class Sprites:
@@ -126,6 +120,7 @@ class Player(Sprites):
     def draw_main(self):
         screen.blit(bg_image, (0, 0))
         screen.blit(ground_image, (self.gr_x, 640 - GROUND_H))
+        screen.blit(ground_image, (self.gr_x - 1024, 640 - GROUND_H))
         # self.len_gr = self.gr_x
         # width = 1024
 
@@ -155,93 +150,69 @@ class Tomb(Sprites):
             self.is_out = True
 
 
-# class Coins(Sprites):
-#     def __init__(self):
-#         super().__init__(coin_image)
-#         self.respawn()
-#
-#     # def spawn(self):
-#     #     self.rect.x = random.randint(400, self.lenght * 1024)
-#     def respawn(self):
-#         self.is_out = False
-#         self.is_dead = False
-#         self.rect.midbottom = (W // 2, H - GROUND_H)
 
-    # def update(self):
-    #     super().update()
-    #     if self.rect.left > W or self.rect.right < 0:
-    #         self.is_out = True
-    # def drawn
+def main():
+    player = Player()
+    score = 0
+    enemies = []
+    INIT_DELAY = 2000 # задержка спавна мобов
+    spawn_delay = INIT_DELAY # как часто спавнятся мобы(интервал)
+    INCREASE_BASE = 1.01
+    last_spawn_time = pygame.time.get_ticks()
 
+    running = True
 
-player = Player()
-score = 0
-# coins = []
-enemies = []
-INIT_DELAY = 2000 # задержка спавна мобов
-spawn_delay = INIT_DELAY # как часто спавнятся мобы(интервал)
-INCREASE_BASE = 1.01
-last_spawn_time = pygame.time.get_ticks()
+    while running:
+        player.draw_main()
 
-running = True
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                running = False
+            elif e.type == pygame.KEYDOWN:
+                if player.is_out:
+                    score = 0
+                    spawn_delay = INIT_DELAY
+                    last_spawn_time = pygame.time.get_ticks()
+                    player.respawn()
+                    enemies.clear()
+        clock.tick(fps)
+        score_text = font_large.render(str(score), True, (255, 255, 255))
+        score_rect = score_text.get_rect()
 
-while running:
-    player.draw_main()
+        if player.is_out:
+            score_rect.midbottom = (W // 2, H // 2)
 
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            running = False
-        elif e.type == pygame.KEYDOWN:
-            if player.is_out:
-                score = 0
-                spawn_delay = INIT_DELAY
-                last_spawn_time = pygame.time.get_ticks()
-                player.respawn()
-                enemies.clear()
-    clock.tick(fps)
-    score_text = font_large.render(str(score), True, (255, 255, 255))
-    score_rect = score_text.get_rect()
-    # coin = Coins()
-    # coin.draw(screen)
-    # for i in range(5):
-    #     screen.blit(coin_image, (random.randint(40, 1000), 500))
-    # if len(coins) == 0:
-    #     coins.append(pygame.Rect(600, 450, 50, 50))
-    #     coins.append(pygame.Rect(700, 450,  50, 50))
-    # for coin in coins:
-    #     pygame.draw.rect(screen, pygame.Color('Yellow'), coin)
-    # for coin in coins:
-    #     coin.x -= 2
-    if player.is_out:
-        score_rect.midbottom = (W // 2, H // 2)
+            screen.blit(retry_text, retry_rect)
+        else: # player is still alive
+            player.update()
+            player.draw(screen)
 
-        screen.blit(retry_text, retry_rect)
-    else: # player is still alive
-        player.update()
-        player.draw(screen)
-
-        now = pygame.time.get_ticks()
-        time_passed = now - last_spawn_time
-        if time_passed > spawn_delay:
-            last_spawn_time = now
-            enemies.append(Tomb())
-        for enemy in list(enemies):
-            if enemy.is_out:
-                enemies.remove(enemy)
-            else:
-                enemy.update()
-                enemy.draw(screen)
-
-            if not player.is_dead and not enemy.is_dead and player.rect.colliderect(enemy.rect):
-                if player.rect.bottom - player.y_speed < enemy.rect.top:
-                    enemy.kill(enemy_dead_image)
-                    player.jump()
-                    score += 1
-                    spawn_delay = INIT_DELAY * 10
+            now = pygame.time.get_ticks()
+            time_passed = now - last_spawn_time
+            if time_passed > spawn_delay:
+                last_spawn_time = now
+                enemies.append(Tomb())
+            for enemy in list(enemies):
+                if enemy.is_out:
+                    enemies.remove(enemy)
                 else:
-                    player.kill(player_image)
+                    enemy.update()
+                    enemy.draw(screen)
 
-        score_rect.midtop = (W // 2, 5)
-    screen.blit(score_text, score_rect)
-    pygame.display.flip()
+                if not player.is_dead and not enemy.is_dead and player.rect.colliderect(enemy.rect):
+                    if player.rect.bottom - player.y_speed < enemy.rect.top:
+                        enemy.kill(enemy_dead_image)
+                        player.jump()
+                        score += 1
+                        spawn_delay = INIT_DELAY / (INCREASE_BASE ** score)
+
+                    else:
+                        player.kill(player_image)
+
+            score_rect.midtop = (W // 2, 5)
+        screen.blit(score_text, score_rect)
+        pygame.display.flip()
+
+
+main()
 quit()
